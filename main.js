f8b081424891b9130ce09d89fe680748898adf6d
asissuban <ignasiusbolisuban@gmail.com>
2025-09-25 05:36:43 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", ()=>{
+    
+    if(window.scrollY > 0){
+
+        navbar.classList.add("scroll-nav");
+    }
+    else{
+        navbar.classList.remove("scroll-nav");
+    }
+})
